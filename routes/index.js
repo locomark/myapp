1e847a3f373579740514b902efa33d92c8f82083
Mark LoSacco <Mark.LoSacco@ca.com>
2015-03-17 17:02:37 -0700

    Updated routes to include PUT, DELETE, POST

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', creator: 'Mark LoSacco', createdate: 'Mar 17, 2015' });
 });
 
+router.post('/', function(req, res, next) {
+  res.send('This is a post');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('This is a put');
+});
+
+router.delete('/', function(req, res, next) {
+  res.send('This is a delete');
+});
+
 module.exports = router;
